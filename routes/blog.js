9d95abee90fcfba1ee790f333670f21e8312cd3a
Mark Mwima <markmwimar@gmail.com>
2023-10-29 21:45:30 +0300

    post details route

--- routes/blog.js
+++ routes/blog.js
@@ -40,4 +40,19 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
+router.get("/posts/:id", async function (req, res) {
+  const query = `
+  SELECT posts.*, authors.name AS author_name authors.email AS author_email FROM posts
+  INNER JOIN authors ON posts.author_id= authors.id 
+  
+  WHERE id=?
+  `;
+  const [posts] = await dbase.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  }
+  res.render("post-detail", { posts: posts[0] });
+});
+
 module.exports = router;
